7e4b9d786044453ab497c835d0a1e146008e67e3
Yann B <ybycode@gmail.com>
2022-02-19 09:28:29 +0100

    mark_as_done.sh now also commits

--- mark_as_done.sh
+++ mark_as_done.sh
@@ -3,6 +3,9 @@
 # Renames a given filename to mark is as 'done', for instance
 # from pages/page-003.todo.txt to pages/page-003.done.txt.
 # Files named *.done.txt are ignored by the `sed_all.sh` script.
+# It then creates a commit for this change.
+
+set -euo pipefail
 
 if [ $# -ne 1 ]; then
   >&2 echo "USAGE EXAMPLE: $0 pages/page-019.todo.txt"
@@ -10,5 +13,9 @@ if [ $# -ne 1 ]; then
 fi
 
 orig="$1"
-dest="${orig%.todo.txt}".done.txt
+base="${orig%.todo.txt}"
+dest="$base.done.txt"
+
 mv "$orig" "$dest"
+git add "$orig" "$dest"
+git commit -m "$base done"
